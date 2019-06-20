90f2f5df58ab08c342b628e21a0bbf733e4ccf5e
sabeat <sabeat2233@gmail.com>
2019-06-20 15:24:43 +0300

    testamonial style

--- front-page.php
+++ front-page.php
@@ -1,43 +1,41 @@
 <?php
 get_header();
-$en   = [
-	'home'        => [
-		'paragraph' => 'laptop and mac book',
-		'big-title' => 'TECHNICAL ASSISTANCE',
-		'button'    => 'learn more',
-	],
-	'service'     => [
-		'maintenance' => [
-			'title'       => 'Maintenance',
-			'description' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
-		],
-		'buy'         => [
-			'title'       => 'Buy Laptops',
-			'description' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
-		]
-	],
-	'about'       => [
-		'title'       => 'About Us',
-		'attention-p' => 'We Are Creative Company',
-		'paragraph'   => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque distinctio ducimus maiores modi saepe. Amet, cumque dolorem, est harum in ipsam iusto officia, pariatur quidem sed sequi unde vero voluptatem. Lorem ipsum dolor sit amet, consectetur adipisiciodi saepe. Amet, cumque dolorem, est harum in ipsam iusto officia, pariatur quidem sed sequi unde vero voluptatem.'
-	],
-	'opening'     => [
-		'title'       => 'Opening Hours',
-		'paragraph-1' => 'From Sept to Fri : from 09hrs to 18hrs',
-		'paragraph-2' => 'Sturday: 09hrs to 15hrs',
-		'paragraph-3' => 'Sunday: closed'
-	],
-	'testimonial' => [
-		'title' => 'Testimonial',
-	],
-	'footer'      => [
-		'copyright'           => 'Copyright',
-		'powred by'           => 'Powred By',
-		'all rights reserved' => 'ALL RIGHTS RESERVED.',
-	]
+$en = [
+    'home' => [
+        'paragraph' => 'laptop and mac book',
+        'big-title' => 'TECHNICAL ASSISTANCE',
+        'button' => 'learn more',
+    ],
+    'service' => [
+        'maintenance' => [
+            'title' => 'Maintenance',
+            'description' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
+        ],
+        'buy' => [
+            'title' => 'Buy Laptops',
+            'description' => 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
+        ]
+    ],
+    'about' => [
+        'title' => 'About Us',
+        'attention-p' => 'We Are Creative Company',
+        'paragraph' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque distinctio ducimus maiores modi saepe. Amet, cumque dolorem, est harum in ipsam iusto officia, pariatur quidem sed sequi unde vero voluptatem. Lorem ipsum dolor sit amet, consectetur adipisiciodi saepe. Amet, cumque dolorem, est harum in ipsam iusto officia, pariatur quidem sed sequi unde vero voluptatem.'
+    ],
+    'opening' => [
+        'title' => 'Opening Hours',
+        'paragraph-1' => 'From Sept to Fri : from 09hrs to 18hrs',
+        'paragraph-2' => 'Sturday: 09hrs to 15hrs',
+        'paragraph-3' => 'Sunday: closed'
+    ],
+    'testimonial' => [
+        'title' => 'Testimonial',
+    ],
+    'footer' => [
+        'copyright' => 'Copyright',
+        'powred by' => 'Powred By',
+        'all rights reserved' => 'ALL RIGHTS RESERVED.',
+    ]
 ];
-$br   = [];
-$lang = $en;
 
 ?>
 <!--Start Header Section-->
@@ -45,10 +43,10 @@ $lang = $en;
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <p      class="wow fadeInDown" data-wow-delay="0s"><?php echo $lang['home']['paragraph'] ?></p>
-                <h1     class="wow fadeInDown" data-wow-delay=".2s"><?php echo $lang['home']['big-title'] ?></h1>
-                <hr     class="wow fadeIn"     data-wow-delay=".5s">
-                <button class="wow fadeInUp"   data-wow-delay=".5s"><?php echo $lang['home']['button'] ?></button>
+                <p class="wow fadeInDown" data-wow-delay="0s">laptop and mac book</p>
+                <h1 class="wow fadeInDown" data-wow-delay=".2s">TECHNICAL ASSISTANCE</h1>
+                <hr class="wow fadeIn" data-wow-delay=".5s">
+                <button class="wow fadeInUp" data-wow-delay=".5s">learn more</button>
             </div>
         </div>
     </div>
@@ -63,9 +61,9 @@ $lang = $en;
                  src="<?php echo get_template_directory_uri() . '/img/Laptop-repairing-service.jpg' ?>"
                  alt="img" width="287" height="192">
             <div class="s-card-body">
-                <h5 class="s-card-title wow zoomInDown" data-wow-duration="1.5s"><?php echo $lang['service']['maintenance']['title'] ?></h5>
+                <h5 class="s-card-title wow zoomInDown" data-wow-duration="1.5s">Maintenance</h5>
                 <p class="s-card-text wow zoomInDown" data-wow-duration="1.5s" data-wow-delay=".3s">
-					<?php echo $lang['service']['maintenance']['description'] ?>
+                    Some quick example text to build on the card title and make up the bulk of the card\'s content.
                 </p>
             </div>
         </div>
@@ -73,9 +71,9 @@ $lang = $en;
             <img class="s-card-img wow fadeInRight" src="<?php echo get_template_directory_uri() . '/img/laptop.png' ?>"
                  alt="img" width="287" height="192">
             <div class="s-card-body">
-                <h5 class="s-card-title wow zoomInDown" data-wow-duration="1.5s"><?php echo $lang['service']['buy']['title'] ?></h5>
+                <h5 class="s-card-title wow zoomInDown" data-wow-duration="1.5s">Buy Laptops</h5>
                 <p class="s-card-text wow zoomInDown" data-wow-duration="1.5s" data-wow-delay=".3s">
-					<?php echo $lang['service']['buy']['description'] ?>
+                    Some quick example text to build on the card title and make up the bulk of the card\'s content.
                 </p>
             </div>
         </div>
@@ -86,11 +84,16 @@ $lang = $en;
 <!--Start About-Us Section-->
 <div class="about-us text-center " id="about-us">
     <div class="container">
-        <h2 class="wow bounceIn"><?php echo $lang['about']['title'] ?></h2>
+        <h2 class="wow bounceIn">About Us</h2>
         <div class="wow flipInX" data-wow-delay=".5s">
-            <p><?php echo $lang['about']['attention-p'] ?></p>
+            <p>We Are Creative Company</p>
             <hr>
-            <p><?php echo $lang['about']['paragraph'] ?></p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a nulla felis. Nullam id metus gravida,
+                gravida nulla non, ultrices justo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec
+                vel velit tempor, varius orci quis, consequat nunc. Suspendisse laoreet dictum fermentum. Aenean quis
+                Sed eget hendrerit lorem, ut vestibulum arcu. Fusce elementum vehicula odio et accumsan.
+            </p>
         </div>
     </div>
 </div>
@@ -99,7 +102,7 @@ $lang = $en;
 <!--Start Opening-Hours Section-->
 <div class="open-hours text-center" id="open-hours">
     <div class="container">
-        <h2 class="text-center wow flipInX"><?php echo $lang['opening']['title'] ?></h2>
+        <h2 class="text-center wow flipInX">Opening Hours</h2>
 
         <div class="row wow flipInX" data-wow-delay=".5s">
             <div class="col-md-6 img text-md-right align-self-center">
@@ -108,11 +111,11 @@ $lang = $en;
             </div>
             <div class="col-md-6 text text-md-left pt-md-5 py-md-4 py-sm-2">
                 <p>
-					<?php echo $lang['opening']['paragraph-1'] ?>
+                    From Sept to Fri : from 09hrs to 18hrs
                     <br>
-					<?php echo $lang['opening']['paragraph-2'] ?>
+                    Sturday: 09hrs to 15hrs
                     <br>
-					<?php echo $lang['opening']['paragraph-3'] ?>
+                    Sunday: closed
                 </p>
             </div>
 
@@ -123,25 +126,24 @@ $lang = $en;
 
 <!--Start What-Customer-Say Section-->
 <div class="what-say" id="what-say">
-    <h2 class="text-center wow headShake"><?php echo $lang['testimonial']['title'] ?></h2>
+    <h2 class="text-center wow headShake">Testimonial</h2>
     <div class="container wow flipInX" data-wow-delay=".7s">
         <hr>
-        <p class="read_more text-center"><a href="<?php echo get_permalink( get_page_by_path('testimonial')->ID ) ?>">Read More >></a></p>
-        <div class="row" >
+        <div class="row">
             <div class="col-md-12">
-                <?php 
-                    // Get Page By Name
-                    $page = get_page_by_path( 'Testimonail-Page' );
-                    if ($page->comment_count !== 0) { ?>
+                <?php
+                // Get Page By Name
+                $page = get_page_by_path('Testimonail-Page');
+                if ($page->comment_count !== 0) { ?>
                     <div id="testimonial-slider" class="owl-carousel">
-						<?php
-						$arg = array(
+                        <?php
+                        $arg = [
                             'post_id' => $page->ID,
-							'number' => 3,  // Number On Display Comments
-							'status' => 'all'
-						);
-						foreach ( get_comments( $arg ) as $comment ) {
-							?>
+                            'number' => 3,  // Number On Display Comments
+                            'status' => 'all'
+                        ];
+                        foreach (get_comments($arg) as $comment) {
+                            ?>
                             <div class="testimonial">
                                 <div class="testimonial-content">
                                     <div class="testimonial-icon">
@@ -151,22 +153,27 @@ $lang = $en;
                                     <!--5<i class="fa fa-star"></i>-->
                                     <!--</div>-->
                                     <p class="description">
-										<?php echo $comment->comment_content ?>
+                                        <?php echo $comment->comment_content ?>
                                     </p>
                                 </div>
                                 <h3 class="title"><?php echo $comment->comment_author ?></h3>
                                 <span class="post"><?php echo $comment->comment_date ?></span>
                             </div>
-							<?php
-						}
+                            <?php
+                        }
 
-						?>
+                        ?>
                     </div>
-					<?php
-				} else {
-					echo '<div class="col alert alert-secondary text-center mb-5">There Is Nothing To Show</div>';
-				} ?>
+                    <?php
+                } else {
+                    echo '<div class="col alert alert-secondary text-center mb-5">There Is Nothing To Show</div>';
+                } ?>
             </div>
+            <p class="read_more text-center" style="position: relative;">
+                <a class="read-more" href="<?php echo get_permalink(get_page_by_path('testimonial')->ID) ?>">Read More
+                    >></a>
+            </p>
+
         </div>
     </div>
 </div>
@@ -229,10 +236,14 @@ echo '<div class="col alert alert-secondary text-center ">There Is Nothing To Sh
     <div class="container">
         <div class="col-md-8 mx-auto text-center">
             <div class="row">
-                <a href="#" class="col d-flex social mb-2 facebook wow zoomInLeft" data-wow-delay=".1s"><i class="fab fa-facebook-square fa-lg p-2"></i>facebook</a>
-                <a href="#" class="col d-flex social mb-2 instagram wow zoomInLeft" data-wow-delay=".3s"><i class="fab fa-instagram fa-lg p-2"></i>instagram</a>
-                <a href="#" class="col d-flex social mb-2 twitter wow zoomInLeft" data-wow-delay=".5s"><i class="fab fa-twitter fa-lg p-2"></i>twitter</a>
-                <a href="#" class="col d-flex social mb-2 linkedin wow zoomInLeft" data-wow-delay=".7s"><i class="fab fa-linkedin fa-lg p-2"></i>linkedin</a>
+                <a href="#" class="col d-flex social mb-2 facebook wow zoomInLeft" data-wow-delay=".1s"><i
+                            class="fab fa-facebook-square fa-lg p-2"></i>facebook</a>
+                <a href="#" class="col d-flex social mb-2 instagram wow zoomInLeft" data-wow-delay=".3s"><i
+                            class="fab fa-instagram fa-lg p-2"></i>instagram</a>
+                <a href="#" class="col d-flex social mb-2 twitter wow zoomInLeft" data-wow-delay=".5s"><i
+                            class="fab fa-twitter fa-lg p-2"></i>twitter</a>
+                <a href="#" class="col d-flex social mb-2 linkedin wow zoomInLeft" data-wow-delay=".7s"><i
+                            class="fab fa-linkedin fa-lg p-2"></i>linkedin</a>
             </div>
         </div>
     </div>
